37abcbf98a63a007b5b9a81ead0c6fb8ff2fc0bd
swayam143 <shubhamswl@gmail.com>
2022-04-16 12:15:34 +0530

    first commit

---
+++ src/AnotherComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnotherComponent = () => {
+  return <div className="primary">AnotherComponent</div>;
+};
+
+export default AnotherComponent;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AnotherComponent from "./AnotherComponent";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <button onClick={switchTheme}>Change theme</button>
+      <AnotherComponent />
     </div>
   );
 }
